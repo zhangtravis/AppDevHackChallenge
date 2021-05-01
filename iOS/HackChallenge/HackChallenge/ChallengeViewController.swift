7679c9d361b2a780ee99c121ca0fd375028e0679
Samantha Zhang <sjz46@cornell.edu>
2021-05-01 17:34:59 -0400

    Set up XCode files and UITabBarController to navigate to different screens

---
+++ iOS/HackChallenge/HackChallenge/ChallengeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChallengeViewController.swift
+//  HackChallenge
+//
+//  Created by Samantha Zhang on 5/1/21.
+//
+
+import UIKit
+
+class ChallengeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
---
+++ iOS/HackChallenge/HackChallenge/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  HackChallenge
+//
+//  Created by Samantha Zhang on 5/1/21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
