e52929387a7027c8569bc64d49563d75b35124db
Travis Zhang <traviszhang2002@gmail.com>
2021-05-09 14:42:45 -0400

    Updated port

--- app.py
+++ app.py
@@ -285,6 +285,6 @@ def upload():
 
 
 if __name__ == "__main__":
-    port = os.environ.get('PORT', 500)
+    port = os.environ.get('PORT', 5000)
     app.run(host="0.0.0.0", port=port)
     # app.run(host="0.0.0.0", port=5000, debug=True)
